b0e84f45d144c54c87fa1388302c3bd8ef0dbbd8
dorobis <dorobis@gmail.com>
2015-12-30 15:14:50 -0500

    JSF 2.2 educational project

---
+++ JSF 2.2 Project/src/com/tutorial/LoginBean.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package com.tutorial;
+
+/**
+ * @author p6248e9
+ *
+ */
+public class LoginBean {
+	public LoginBean() {
+	}
+
+	private String name;
+	private String password;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(final String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(final String password) {
+		this.password = password;
+	}
+}
